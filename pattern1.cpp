fbbcbcd5820b29af514bcd1e1f53a543799a216f
Md Aamir <asadahmadkh344@gmail.com>
2021-10-11 14:07:57 +0530

    pattern

--- pattern1.cpp
+++ pattern1.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main()
 {
-   int row,col;
-   cin>>row>>col;
-   for(int i=0;i<=row;i++)
+  int row,col;
+  cin>>row>>col;
+   
+   for(int i=1;i<=row;i++)
    {
-       for(int j=0;j<=col;j++)
+       for(int j=1;j<=col;j++)
        {
            cout<<"*";
        }
