9c9227e9e31221ec6cbe0282da17e07f8c25e72f
Md Aamir <asadahmadkh344@gmail.com>
2021-10-12 20:40:49 +0530

    pattern question

--- pattern2.cpp
+++ pattern2.cpp
@@ -10,12 +10,13 @@ int main()
        for(int j=1;j<=col;j++)
        {
           
-           if (i==1 || j==1)
+           if (i==1 || i==row || j==1 || j==col)
            {
                cout<<"*";
            }
-           
-           
+           else {
+           cout<<" ";
+           } 
        }
        cout<<endl;
    }
