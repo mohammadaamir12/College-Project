946b10e9ea6e68571ee03deee942e0efea7ec5b6
Md Aamir <asadahmadkh344@gmail.com>
2021-10-14 20:55:13 +0530

    pattern8

---
+++ pattern8.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cin>>n;
+    for( i=1;i<=n;i++){
+        for( j=1;j<=i;j++){
+            cout<<"*";
+        }
+            int space=2*n-2*i;
+           for(j=1;j<=space;j++){
+               cout<<" ";
+           }
+           for(j=1;j<=i;j++){
+               cout<<"*";
+           }
+        
+        cout<<endl;
+    }
+
+    for( i=n;i>=1;i--){
+        for( j=1;j<=i;j++){
+            cout<<"*";
+        }
+            int space=2*n-2*i;
+           for(j=1;j<=space;j++){
+               cout<<" ";
+           }
+           for(j=1;j<=i;j++){
+               cout<<"*";
+           }
+        
+        cout<<endl;
+    }
+
+   
+    return 0;
+}
