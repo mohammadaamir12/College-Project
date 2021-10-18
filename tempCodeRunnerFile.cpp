68fa76647648147d973f64879ae57d2c58f785ae
Md Aamir <asadahmadkh344@gmail.com>
2021-10-18 16:35:25 +0530

    update

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -6,10 +6,11 @@ int main()
   cin>>n;
    
    for(int i=1;i<=n;i++){
-      for(int j=1;j<=n+1-i;j++)  {
-          cout<<j<<" ";
+      for(int j=1;j<=n-i;j++){
+          cout<<" ";
+      }
+      for(int j=1;j<=2*i-1;j++){
+          cout<<"*";
       }
       cout<<endl;
    }
-    return 0;
-}
