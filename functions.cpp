c02ed74cbbf2e621ed60849535ec16f71f5ec219
Md Aamir <asadahmadkh344@gmail.com>
2021-10-20 19:06:49 +0530

    functions

---
+++ functions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=2;
+int b=3;
+cout<<print(a,b)<<endl;
+return 0;
+}
+
+int print(int num1,int num2)
+{
+    
+int sum=num1+num2;
+return sum;
+}
