2f757843b3029c7569c8f81df07c40183a83d2fb
Md Aamir <asadahmadkh344@gmail.com>
2021-10-21 21:39:35 +0530

    Primebyboolean

---
+++ function4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+bool isPrime(int num){
+    for(int i=2;i<=sqrt(num);i++){
+        if(num%2==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int a;
+    int b;
+    cin>>a>>b;
+    for(int i=a;i<=b;i++)
+    {
+        if(isPrime(i)){
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
