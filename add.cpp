d8a488faeefdf1f9f0834de9299c5e41d06d62ac
Md Aamir <asadahmadkh344@gmail.com>
2021-10-24 22:58:29 +0530

    add

---
+++ add.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int add(int n)
+{
+    
+    int ans=0;
+    for(int j=0;j<=n;j++){
+        ans+=j;
+    }
+    
+    return ans;
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+   
+   cout<<add(n);
+    return 0;
+}
