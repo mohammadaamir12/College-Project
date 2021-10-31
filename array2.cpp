1a6e140e09930af04e0cf923cc8c892256c987f4
Md Aamir <asadahmadkh344@gmail.com>
2021-10-31 19:03:13 +0530

    array

---
+++ array2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a;
+    cin>>a;
+    int arr[a];
+    for(int i=0;i<arr[a];i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<arr[3]<<endl;
+    return 0;
+
+}
