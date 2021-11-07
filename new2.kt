abc7c2502dc54c004625dcd83f07551b7591756e
Md Aamir <asadahmadkh344@gmail.com>
2021-11-07 19:05:27 +0530

    new2

---
+++ new2.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    val number1 = 12.5
+    val number2 = 3.5
+    var result: Double
+
+    result = number1 + number2
+    println("number1 + number2 = $result")
+
+    result = number1 - number2
+    println("number1 - number2 = $result")
+
+    result = number1 * number2
+    println("number1 * number2 = $result")
+
+    result = number1 / number2
+    println("number1 / number2 = $result")
+
+    result = number1 % number2
+    println("number1 % number2 = $result")
+}
