6246e63a843dcc3bc231ec025bbad22709397644
Md Aamir <asadahmadkh344@gmail.com>
2021-11-08 11:59:33 +0530

    java hello world

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,24 @@
 #include<iostream>
 using namespace std;
+
+int fac(int num)
+{
+    
+    int fac=1;
+    
+    for(int i=2;i<=num;i++)
+    {
+        fac*=i;
+    }
+    return fac;
+}
+
+
 int main()
 {
-  int n;
-  cin>>n;
-   
-   for(int i=1;i<=n;i++){
-      for(int j=1;j<=n-i;j++){
-          cout<<" ";
-      }
-      for(int j=1;j<=2*i-1;j++){
-          cout<<"*";
-      }
-      cout<<endl;
-   }
+    int n,r;
+    cin>>n>>r;
+   int ans=fac(n)/(fac(r)*fac(n-r));
+   cout<<ans;
+    return 0;
+}
