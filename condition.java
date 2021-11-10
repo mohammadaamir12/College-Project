565dacdcf7f1f8288d842eb570bf7ee1ec6bc229
Md Aamir <asadahmadkh344@gmail.com>
2021-11-10 10:45:39 +0530

    basic

--- condition.java
+++ condition.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class condition {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int age=sc.nextInt();
+    public static void main(String args []){
+        Scanner in =new Scanner(System.in);
+        int age=in.nextInt();
 
         if(age<=18){
             System.out.println("Not Adult");
