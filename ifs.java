3d3fca2826a612bbe1c7c81db4d522a3fdb7d752
Md Aamir <asadahmadkh344@gmail.com>
2021-11-11 20:26:14 +0530

    ifelse

---
+++ ifs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+ class ifs {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+       if(a==1)
+       {
+           System.out.println("hello");
+       }
+       else if (a==2){
+            System.out.println("namaste");
+       }
+       else if(a==3){
+           System.out.println("bonjour");
+       }
+     else{
+         System.out.println("invalid");
+     }
+
+    }
+}
+
