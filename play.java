efd7cd1027364b76c7bc135e9be4c405fcf2da51
Md Aamir <asadahmadkh344@gmail.com>
2021-11-16 20:16:59 +0530

    play with array

---
+++ play.java
@@ -0,0 +1,20 @@
+
+
+
+public class play{
+public static void main(String args []){
+    
+ 
+ int number[]={2,4,6};
+ for(int i=0;i<3;i++){
+System.out.println(number[i]);
+
+ }
+  
+       
+    
+    
+
+}
+
+}
