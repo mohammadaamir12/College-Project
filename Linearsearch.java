3bad4759f202da12b0320f19afb30aec58fe7930
Md Aamir <asadahmadkh344@gmail.com>
2021-11-16 20:07:58 +0530

    Linearsearch

--- Linearsearch.java
+++ Linearsearch.java
@@ -1,21 +1,22 @@
 import java.util.*;
 
 
-public class linear{
+public class linearsearch {
 public static void main(String args []){
     int i=0;
  Scanner sc=new Scanner(System.in);
  int size=sc.nextInt();
+ int x=sc.nextInt();
  int number[]=new int[size];
 for( i=0;i<size;i++){
    number[i]=sc.nextInt();
 
 }
 for( i=0;i<size;i++){
-    
+    if(number[i]==x){
         System.out.println("found at index" + i);
     
-    
+    }
 }
 }
 
