82aa085a151e73107b948569c89505e6af376a5e
Md Aamir <asadahmadkh344@gmail.com>
2021-11-16 20:03:10 +0530

    linear

---
+++ linear.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class linear{
+public static void main(String args []){
+    int i=0;
+ Scanner sc=new Scanner(System.in);
+ int size=sc.nextInt();
+ int number[]=new int[size];
+for( i=0;i<size;i++){
+   number[i]=sc.nextInt();
+
+}
+for( i=0;i<size;i++){
+    
+        System.out.println("found at index" + i);
+    
+
+}
+}
+
+}
