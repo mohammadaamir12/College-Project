9c013dff46a622c918022e0c9a397a21d9c70a82
Md Aamir <asadahmadkh344@gmail.com>
2021-11-27 12:23:02 +0530

    newww

---
+++ newww.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.*;
+ class newww {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s=scan.nextLine();
+        
+
+    
+
+        System.out.println("String: " + s);
+    }   
+}
