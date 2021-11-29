096bd75acf1602a0c414e03919123237d74f46a4
Md Aamir <asadahmadkh344@gmail.com>
2021-11-29 16:07:40 +0530

    math

---
+++ math.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+public class math{
+     
+
+    public static void main(String[] args) {
+        int a=5;
+        int b= 0;
+        b=(math.random+a);
+        System.out.println(" this is "+ b);
+
+        
+    }
+
+    
+}
