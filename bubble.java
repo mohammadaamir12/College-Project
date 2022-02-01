65d59ca7fc432da01a52c89bc0a820556049a456
Md Aamir <asadahmadkh344@gmail.com>
2022-02-01 10:37:55 +0530

    bubblesort

---
+++ bubble.java
@@ -0,0 +1,22 @@
+public class bubble{
+
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] +" ");
+        }
+    }
+public static void main(String args[]) {
+int arr[]={7,8,5,1,2};
+
+for(int i=0;i<arr.length-1;i++){
+    for(int j=0;j<arr.length-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+}
+    printArray(arr);
+}
+}
